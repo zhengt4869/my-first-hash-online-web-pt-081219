db6cbee9d1f513a83d6ff9a5971647f3d98938b9
KG ZZ <czheng5h88@gmail.com>
2019-09-11 14:53:34 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -13,7 +13,7 @@ def shipping_manifest
   the_manifest["whale bone corsets"] = 5
   the_manifest["porcelain vases"] = 2 
   the_manifest["oil paintings"] = 3 
-
+  the_manifest
 end
 
 def retrieval
