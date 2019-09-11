aafb27d549d4620e4392d6381c8304c9a9284e2a
KG ZZ <czheng5h88@gmail.com>
2019-09-11 14:52:37 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -39,7 +39,7 @@ def adding
 
   # add 4 gun powder to the shipping_manifest hash below
 
-  shipping_manifest[""]
+  shipping_manifest["gun powder"] = 4 
 
   # return the shipping_manifest hash below
 
